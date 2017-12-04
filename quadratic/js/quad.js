4cad3a0fe6898fa5f854ee9ec7ea37af23abe168
Charles Zhang <icharliezhang@gmail.com>
2017-12-03 21:54:42 -0500

    Initial addition of Quadratic Equation Calculator for Mr Harriz

---
+++ quadratic/js/quad.js
@@ -0,0 +1,16 @@
+"use strict";
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+ 
+let a, b, c
+let discriminant = b ** 2 - (4 * a * c)
+if (discriminant >= 0){
+	let x1 = (-b+ sqrt(discriminant)/(2 * a) // quadratic equation solution 1
+	let x2 = (-b+ sqrt(discriminant)/(2 * a) // solution 2
+}
